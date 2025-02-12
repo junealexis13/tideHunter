d84844056f43eaa2b88b7b21844636e687e46308
junealexis13 <junealexis.santos13@gmail.com>
2025-02-12 16:59:14 +0800

    added footer

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from page_design import AppWidgets
 def main():
     a = AppWidgets()
     a.body()
-
+    st.write("Developed and designed by: _:blue[JunnieBoy13]_ ")
 
 if __name__ == "__main__":
     main()
